2176ba3daaa2df655dfb6b6a6ce9297e85f7a88c
Raphael Miranda <raphaelmiranda@gmail.com>
2012-08-27 00:05:23 -0300

    cleanup

--- src/main/java/mailsync/MailSyncMain.java
+++ src/main/java/mailsync/MailSyncMain.java
@@ -13,8 +13,8 @@ public class MailSyncMain {
 	
     public static void main(String[] args) throws IOException, MessagingException {
     	
-    	SessionManager sm = new SessionManager("SquadMail","imap.gmail.com","raphaelmiranda@gmail.com",System.getProperty("gmailpass"));
-    	SessionManager sm2 = new SessionManager("SquadMail","imap.gmail.com","raphael@gimajam.com",System.getProperty("gimapass"));
+    	SessionManager sm = new SessionManager("SquadMail","imap.gmail.com",System.getProperty("gmailaddress"),System.getProperty("gmailpass"));
+    	SessionManager sm2 = new SessionManager("SquadMail","imap.gmail.com",System.getProperty("gmailaddress2"),System.getProperty("gmailpass2"));
     	FolderManager fm = new FolderManager();
     	
     	fm.synchronizeFolders(sm.getImapFolder(), sm2.getImapFolder());
